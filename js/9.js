04bb452d7869c6de4ba9e5db10d2cef94b5ed3f6
Nikita Vasilenkov <n.vasilenkov@tinkoff.ru>
2024-06-23 23:35:07 +0300

    feature/23-06 add

--- js/9.js
+++ js/9.js
@@ -1,3 +1,4 @@
+// Exes and Ohs
 // Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
 
 // Examples input/output:
